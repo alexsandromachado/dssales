478bb116da56029714534f63eeeebdcd058c0f10
Alexsandro Machado <lekomachado@hotmail.com>
2022-08-05 16:08:11 -0300

    SalesSum backend integration, currency number format

---
+++ front-web/src/utils/formatters.ts
@@ -0,0 +1,7 @@
+export const formatPrce = (price: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    minimumFractionDigits: 2,
+    style: 'currency',
+    currency: 'BRL'
+  }).format(price);
+};
